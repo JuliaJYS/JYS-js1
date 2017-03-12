748335c64226487492b79416274fcbe27ad7fb0a
JuliaJYS <julysa83@gmail.com>
2017-03-12 19:18:59 +0300

    все задания по ДЗ№1.

---
+++ lesson1.js
@@ -0,0 +1,15 @@
+//1 задание
+var tC = 45;
+var tF = (9 / 5)*tC + 32;
+alert('Температура по Фаренгейту ' + tF);
+//2+3 задание
+var name;
+var admin;
+name = 'Василий';
+admin = name;
+alert(admin);
+//4 задание - ответ 1000108
+var valueNumber = 1000;
+var valueString = '108';
+alert(valueNumber + valueString);
+
