560a76b44fa1ef38ffcbec73849640012c083436
JuliaJYS <julysa83@gmail.com>
2017-03-25 10:52:55 +0300

    Урок 3. Все ДЗ
    проверяла в консоли

--- lesson3.js
+++ lesson3.js
@@ -2,7 +2,40 @@
 var n = 100;
 var i = 0;
 while (i <= n) {
-console.log(i++);
+	console.log(i++);
 }
 
 //2.
+var n = 10;
+var i = 0;
+do{
+	if (i == 0) {
+		console.log(i++ +' - это ноль');
+	} 
+	else if (parseInt(i) % 2 == 0) {
+		console.log(i++ +' - четное число');
+	}
+	else {
+		console.log(i++ +' - нечетное число');
+	}
+		
+} while (i <= n);
+
+//3.
+var n = 10;
+var i = 0;
+for(i; i < n; console.log(i++)){}
+
+//4.
+var n = 5;
+var i = 1;
+var a = 1;
+var x = 'X';
+for(i; i <= n; i++) {
+	for(a; a < i; a++){
+		x = x + 'X';
+	}
+	console.log(x);
+}
+
+
