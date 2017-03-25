3984e1be09f16d601047eb3763352c6a3cc5161f
JuliaJYS <julysa83@gmail.com>
2017-03-25 11:00:38 +0300

    Урок 3_3
    Итегрировала табуляцию Sublime text в 4 пробела.

--- lesson3.js
+++ lesson3.js
@@ -2,23 +2,23 @@
 var n = 100;
 var i = 0;
 while (i <= n) {
-	console.log(i++);
+    console.log(i++);
 }
 
 //2.
 var n = 10;
 var i = 0;
 do{
-	if (i == 0) {
-		console.log(i++ +' - это ноль');
-	} 
-	else if (parseInt(i) % 2 == 0) {
-		console.log(i++ +' - четное число');
-	}
-	else {
-		console.log(i++ +' - нечетное число');
-	}
-		
+    if (i == 0) {
+        console.log(i++ +' - это ноль');
+    } 
+    else if (parseInt(i) % 2 == 0) {
+        console.log(i++ +' - четное число');
+    }
+    else {
+        console.log(i++ +' - нечетное число');
+    }
+        
 } while (i <= n);
 
 //3.
@@ -32,10 +32,10 @@ var i = 1;
 var a = 1;
 var x = 'X';
 for(i; i <= n; i++) {
-	for(a; a < i; a++){
-		x = x + 'X';
-	}
-	console.log(x);
+    for(a; a < i; a++){
+        x = x + 'X';
+    }
+    console.log(x);
 }
 
 
