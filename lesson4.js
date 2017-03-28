cba80d38d2800305f389524ca6db412ab415d52a
JuliaJYS <julysa83@gmail.com>
2017-03-28 17:03:44 +0300

    Добавила вторую задачу по 4 уроку
    Добавила вторую задачу

--- lesson4.js
+++ lesson4.js
@@ -32,3 +32,48 @@ for (var i in objNum) {
 }
 
 //2. 
+var a, x, z, n;
+
+function beginGame() {
+  return prompt('Вы хотете поиграть? 1 - Да, 0 - Нет', a);
+}
+
+function goGame(z) {
+  var hod = 1;
+  x = prompt('Введите число от 0 до 15:', x);
+  while (hod <= z) {
+    for (x; x <= 15; x++) {
+      if ((x < 0) || (x > 15)) {
+        alert('Вы ввели неправильное число!');
+        hod = 0;
+        break;
+      }
+      if (parseInt(x) % 2 == 0) {
+        console.log('Вы ввели четное число, поздравляю вы выйграли!');
+        hod++;
+        break;
+      } 
+      else {
+        hod++;
+      }
+    }
+    return hod;
+  }  
+}
+
+a = beginGame();
+if (a == 1) {
+  console.log('Игра начинается!');
+  z = prompt('Введите число ходов:', z);
+  n = goGame(z);
+  if (n == 0) {
+    console.log('Вы проиграли!');
+  } 
+  else {
+    console.log('Вы выйграли на:' + n + ' ход.');
+  }
+} 
+else {
+  console.log('Вы проиграли!');
+}
+
