3d5464f26815806896f7a925867ebb5aa82ce309
JuliaJYS <julysa83@gmail.com>
2017-04-08 13:18:07 +0300

    Палиндром.
    Исправила пробелы

--- palindrom.js
+++ palindrom.js
@@ -1,13 +1,13 @@
 function isPalindrome (w) {
-	w = w.replace(/,/g,"").replace(/ /g,"").toLowerCase();
-	var z = w.length, i = 0;
-	while (i <= z) {
-		if (w[i] != w[(z-1)-i]) {
-			return false;	
-		}
-		i++;
-	}
-	return true;
+    w=w.replace(/,/g,"").replace(/ /g,"").toLowerCase();
+    var z=w.length,i=0;
+    while (i<=z) {
+        if (w[i]!=w[(z-1)-i]) {
+            return false;   
+        }
+        i++;
+    }
+    return true;
 }
 
 console.log(isPalindrome("level")); // logs 'true'
